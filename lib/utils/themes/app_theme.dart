094dfd92744219324220dddda68179559ef487c4
R-12 <ridwandani12@gmail.com>
2022-06-05 17:32:39 +0700

    update bagian tema, pakai variabel warna dan weight huruf konstan

--- lib/utils/themes/app_theme.dart
+++ lib/utils/themes/app_theme.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
+// Color
+const Color kBackgroundColor = Color.fromRGBO(244, 244, 244, 1);
+const Color kPrimaryColor = Color.fromRGBO(79, 189, 186, 1);
+const Color kSecondaryColor = Color.fromRGBO(26, 46, 57, 1);
+const Color kDarkColor = Color.fromRGBO(14, 14, 14, 1);
+const Color kLightColor = Color.fromRGBO(254, 254, 252, 1);
+const Color kDisabledColor = Color.fromRGBO(175, 175, 175, 1);
+const Color kErrorColor = Color.fromRGBO(141, 41, 51, 1);
+const Color kPressedColor = Color.fromRGBO(40, 95, 93, 1);
+
+// FontWeight
+const FontWeight kRegularWeight = FontWeight.w400;
+const FontWeight kMediumWeight = FontWeight.w500;
+const FontWeight kSemiBoldWeight = FontWeight.w600;
+const FontWeight kBoldWeight = FontWeight.w700;
+
 class AppTheme {
   static ThemeData lightTheme() {
     return ThemeData(
       colorScheme: const ColorScheme.light().copyWith(
-          // untuk edit warna, dll bisa di sini.
-          // ada banyak propertinya
-          ),
-      // Styling untuk input decoration
-      inputDecorationTheme: InputDecorationTheme(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(200),
-        ),
+        primary: kPrimaryColor,
+        onPrimary: kLightColor,
+        secondary: kSecondaryColor,
+        onSecondary: kLightColor,
+        error: kErrorColor,
+        background: kBackgroundColor,
+        onBackground: kDarkColor,
       ),
     );
   }
